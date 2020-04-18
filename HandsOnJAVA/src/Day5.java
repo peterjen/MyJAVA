377592a133c7c11c3d70b4bf2632fa693b3abeb2
Peter Jen <ptjen@yahoo.com>
2020-04-18 14:22:57 -0400

    JAVA Day5

--- HandsOnJAVA/src/Day5.java
+++ HandsOnJAVA/src/Day5.java
@@ -9,15 +9,17 @@ public class Day5 {
 		LinkedList<String> name = new LinkedList<String>();
 		Scanner in = new Scanner(new File("src/students.txt"));
 		while(in.hasNext()) {
-			name.add(in.nextLine());
+			//name.add(in.nextLine());  // ADD => QUEUE  first in first out
+			name.push(in.nextLine());  // PUSH => STACK first in last out
+
 		}
-		
+		System.out.println(name);
 		System.out.println(name.size());
 		
 		System.out.println(name.pop());
 		System.out.println(name);
-		name.push("Uno");
-		System.out.println(name);
+		//name.push("Uno");
+		//System.out.println(name);
 		System.out.println(name.pop());
 		System.out.println(name);
 		System.out.println(name.pop());
