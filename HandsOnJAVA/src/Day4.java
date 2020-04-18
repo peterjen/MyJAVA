e89016b5c85f8e5b06e48713b714c38b1809d225
Peter Jen <ptjen@yahoo.com>
2020-04-18 13:17:49 -0400

    JAVA Day4

--- HandsOnJAVA/src/Day4.java
+++ HandsOnJAVA/src/Day4.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Day4 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner scanner = new Scanner(new File( "D:\\eclipse-workspace\\HandsOnJAVA\\src\\students.txt"));
-		//Scanner scanner = new Scanner(new File( "students.txt"));
+		//Scanner scanner = new Scanner(new File( "D:\\eclipse-workspace\\HandsOnJAVA\\src\\students.txt"));
+		Scanner scanner = new Scanner(new File( "src/students.txt"));
 		ArrayList<String> a = new ArrayList<String>();
 		while(scanner.hasNextLine()) {
 			a.add(scanner.nextLine());
