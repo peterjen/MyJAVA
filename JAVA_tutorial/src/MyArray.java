d4c5cb6594803b4405eddd4077bb2cd8ffd1287b
Peter Jen <ptjen@yahoo.com>
2020-04-18 21:13:52 -0400

    initial commit

--- JAVA_tutorial/src/MyArray.java
+++ JAVA_tutorial/src/MyArray.java
@@ -3,9 +3,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
-
-import javax.swing.event.ListSelectionEvent;
 
 public class MyArray {
 
