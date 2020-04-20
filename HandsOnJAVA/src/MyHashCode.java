9e97a682f9926024da68d316d26162902f6c1eb0
Peter Jen <ptjen@yahoo.com>
2020-04-20 13:32:13 -0400

    MAZE Day20

---
+++ HandsOnJAVA/src/MyHashCode.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class MyHashCode {
+
+	public static void main(String[] args) {
+		// DAY 20
+		
+		//Object ss = "Hello";
+		//Object tt = "Hello";
+		//System.out.println(ss.hashCode());
+		//System.out.println(tt.hashCode());
+
+		Person p = new Person("ppp@email.com","Peter");
+		Position pos1 = new Position(10,10);
+		p.position = pos1;
+		
+		Person q = new Person("ppp@email.com","Peter");
+		Position pos2 = new Position(10,10);
+		q.position = pos2;
+		System.out.println(p.equals(q));
+		System.out.println(p.hashCode());
+		System.out.println(q.hashCode());
+		
+
+		//DAY 21
+		HashSet<String> words = new HashSet<String>();
+		words.add("hello");
+		words.add("bye");
+		words.add("dog");
+		words.add("cat");
+		words.add("ice");
+		words.add("taco");
+		words.add("fish");
+		words.add("net");
+		for (String s : words) {
+			System.out.println(s);  
+		}
+		System.out.println(words.contains("fish"));
+		
+		HashSet<Person> ppl = new HashSet<Person>();
+		ppl.add(p);
+		ppl.add(q);
+		System.out.println(ppl.contains(p));
+		
+		Person newp = new Person("ppp@email.com","Peter");
+		//Position pos1 = new Position(10,10);
+		newp.position = pos1; // newp is not added to ppl, but ppl contains newp
+		System.out.println("newp == p? " + newp.equals(p));
+		System.out.println("ppl contains newp? " + ppl.contains(newp));
+		
+		HashMap<Integer,String> peek = new HashMap<Integer,String>();
+		peek.put(123, "June");
+		peek.put(456, "John");
+		peek.put(789, "CJ");
+		System.out.println("HashMap peek contains? " + peek.containsKey(123));
+		System.out.println(peek.entrySet());
+	}
+
+}
---
+++ HandsOnJAVA/src/Person.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+public class Person {
+
+	String email;
+	String name;
+	Position position;
+	
+	Person(String email,String name){
+		this.email = email;
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		//System.out.println("aaa");
+		return Objects.hash(email,name,position);
+		/*
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+		*/
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		return true;
+	}
+}
