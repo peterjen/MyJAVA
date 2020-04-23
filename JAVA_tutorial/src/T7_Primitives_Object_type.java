6a194a142254a34121f74873be9171160274e745
Peter Jen <ptjen@yahoo.com>
2020-04-23 18:39:42 -0400

    T7

---
+++ JAVA_tutorial/src/T7_Primitives_Object_type.java
@@ -0,0 +1,12 @@
+
+public class T7_Primitives_Object_type {
+	public static int x = 3;
+	public static void main(String[] args) {
+		int x = 5;			// Primitives Type
+		Integer y = 10;		// Object Type
+		System.out.println(x + y);
+		System.out.println(T7_Primitives_Object_type.x + y);
+
+	}
+
+}
