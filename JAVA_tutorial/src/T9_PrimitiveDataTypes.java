9896499e52f434b0995f33fb79c52be6e023f1d0
Peter Jen <ptjen@yahoo.com>
2020-04-23 19:43:22 -0400

    T9

---
+++ JAVA_tutorial/src/T9_PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+
+public class T9_PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		boolean good = true;
+		byte  b = 'C';
+		char c = 'C';
+		short sh = 32767;
+		int i =222;
+		long lo = 234333333333L;
+		
+		float f = 2.3f;
+		double db = 3.456;  // use double 99.9 %
+		
+		System.out.println(good);
+		System.out.println(b);
+		System.out.println(c);
+		
+		
+
+	}
+
+}
