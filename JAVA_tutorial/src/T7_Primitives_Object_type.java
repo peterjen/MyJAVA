4fe7ffd7519b62e370ceaa3f80955245f4815c99
Peter Jen <ptjen@yahoo.com>
2020-04-23 18:41:10 -0400

    T7

--- JAVA_tutorial/src/T7_Primitives_Object_type.java
+++ JAVA_tutorial/src/T7_Primitives_Object_type.java
@@ -1,6 +1,7 @@
 
 public class T7_Primitives_Object_type {
 	public static int x = 3;
+	//private static int x = 3;
 	public static void main(String[] args) {
 		int x = 5;			// Primitives Type
 		Integer y = 10;		// Object Type
