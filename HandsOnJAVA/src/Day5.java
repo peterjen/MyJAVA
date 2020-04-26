2b367cd5655de3948fa17f799608dea3efd4c9f8
Peter Jen <ptjen@yahoo.com>
2020-04-26 16:06:35 -0400

    JAVA Day5

--- HandsOnJAVA/src/Day5.java
+++ HandsOnJAVA/src/Day5.java
@@ -9,16 +9,19 @@ public class Day5 {
 		LinkedList<String> name = new LinkedList<String>();
 		Scanner in = new Scanner(new File("src/students.txt"));
 		while(in.hasNext()) {
-			//name.add(in.nextLine());  // ADD => QUEUE  first in first out
-			name.push(in.nextLine());  // PUSH => STACK first in last out
+			//name.add(in.nextLine());  // ADD to the end => QUEUE  first in first out
+			name.push(in.nextLine());  // PUSH to the front => STACK first in last out
 
 		}
 		System.out.println(name);
 		System.out.println(name.size());
 		
-		System.out.println(name.pop());
+		System.out.println(name.pop());			// remove first element 
+		//System.out.println(name.remove());   // remove first element same as pop by default.  remove(2) will remove the 2nd element.  remove("Peter") will remove "Peter"
+		//System.out.println(name.removeFirst());  // remove first element  same as pop
 		System.out.println(name);
-		//name.push("Uno");
+		System.exit(0);
+		name.push("Uno");
 		//System.out.println(name);
 		System.out.println(name.pop());
 		System.out.println(name);
