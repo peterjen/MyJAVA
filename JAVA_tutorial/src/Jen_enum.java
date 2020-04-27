202bdaa03974acb0a2f4d9e42fb9e6aea71f5622
Peter Jen <ptjen@yahoo.com>
2020-04-26 21:23:35 -0400

    Classes

---
+++ JAVA_tutorial/src/Jen_enum.java
@@ -0,0 +1,7 @@
+// T88
+public class Jen_enum extends User {
+	public enum T_shirt_size {xs,s,m,l,xl};  // T98 enum 
+	public String name;
+	public T_shirt_size tsize;
+
+}
---
+++ JAVA_tutorial/src/User.java
@@ -0,0 +1,105 @@
+import java.util.List;
+
+public class User {
+
+	private String firstName;
+	public String lastName;
+
+	User (){
+
+	}
+	User(String f, String l){
+		this.firstName = f;
+		this.lastName = l;
+
+	}
+	public String getFullName() {  
+		return this.firstName + " " + lastName;
+	}
+
+	public String getFullName(boolean nice) {
+		if(nice) {
+			return this.firstName + " " + lastName + " is a nice guy.";
+		}
+		return this.firstName + " " + lastName + " is not a nice guy.";
+	}
+
+//  Encapsulation : getter/ setter for PRIVATE field(s)- firstName
+	public void setLastName(String str) {
+		this.lastName = str;
+	}
+	public void setFirstName(String str) {
+		this.firstName = str;
+	}
+
+	public String says(String w) {
+		return firstName + " SAYS " + w ;
+	}
+
+	public void printUser() {							// instance method
+		System.out.println("USER() Instance : " + getFullName());
+	}
+
+	public static void printUser(User u) {				// static method
+		System.out.println("USER() Static : " + u.getFullName());
+	}
+
+	public static void printUserList(List<User> u) {				// static method
+		for (User i : u) {
+			System.out.println("USER() Static : " + i.getFullName());
+		}
+
+	}
+
+	public static int findUserFromList(List<User> ulist,User u) {
+		return findUserFromList(ulist,u.firstName,u.lastName);
+	}
+	public static int findUserFromList(List<User> ulist,String ff, String ll) {
+		for (int i = 0; i < ulist.size(); i++ ) {
+		//for(User u : ulist) {
+				if (ulist.get(i).getFullName().equals(ff + " " + ll)) {
+				//if (u.getFullName().equals(ff + " " + ll)) {
+						return i;
+				}
+		}
+		return -1;
+	}
+
+	@Override
+	public String toString() {
+		return "User @Override toString [getFullName()=" + getFullName() + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+/*
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	*/
+
+}
