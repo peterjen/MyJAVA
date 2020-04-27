f44d4527630f88566894f16e03746bf9a7626d39
Peter Jen <ptjen@yahoo.com>
2020-04-26 21:34:00 -0400

    T18

---
+++ JAVA_tutorial/src/T18_String_Comparison.java
@@ -0,0 +1,20 @@
+
+public class T18_String_Comparison {
+
+	public static void main(String[] args) {
+		String s1 ="asd";
+		String s2 ="asd";
+		String s3 = new String("asd");
+
+		System.out.println("Compare Primitives:::");
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));  // Correct way....
+
+		System.out.println("Compare Objects:::");		
+		System.out.println(s1==s3);
+		System.out.println(s1.equals(s3));
+
+
+	}
+
+}
