d92dbf0cfd8d550afa9642d411ca3e4b39a17aa7
Peter Jen <ptjen@yahoo.com>
2020-04-26 21:29:19 -0400

    Classes

---
+++ JAVA_tutorial/src/People.java
@@ -0,0 +1,18 @@
+public abstract class People implements T95_Talk_Interface{    // T85 ABSTRACT CLASS
+
+
+	public String firstName;
+	public String lastName;
+	public boolean verified = false;
+
+	public void getFullName() {
+		System.out.println(firstName + " " + lastName);
+	}
+
+	public abstract void sayHello();  // T86 ABSTRACT METHOD MUST BE UNDER ABSTRACT CLASS.  The sub class from People MUST have sayHello()
+	//public void sayHello();  // 
+
+	public final void sayHi() {   // T96 final NOT ABLE to be inherited !!!
+		System.out.println("People level says Hi");
+	}
+}
---
+++ JAVA_tutorial/src/T95_Talk_Interface.java
@@ -0,0 +1,4 @@
+// T95 https://www.youtube.com/watch?v=Y3-Q0UX7YeQ&list=PL_c9BZzLwBRKIMP_xNTJxi9lIgQhE51rF&index=96
+public interface T95_Talk_Interface {
+	public void sayWelcome();
+}
