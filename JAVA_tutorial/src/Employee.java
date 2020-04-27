c46a265e17eaf7e4a80e5c5c4a35bf9875507ebf
Peter Jen <ptjen@yahoo.com>
2020-04-26 21:13:32 -0400

    Employee

---
+++ JAVA_tutorial/src/Employee.java
@@ -0,0 +1,25 @@
+public class Employee {
+	public String firstname;
+	public String lastname;
+	
+	public Employee(String f, String l) {
+		this.firstname = f;
+		this.lastname = l;
+	}
+	public String getFullname() {
+		return firstname + " " + lastname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+}
