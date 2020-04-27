c7eeddd7cb0816192fc6ab466bdbc7a814098226
Peter Jen <ptjen@yahoo.com>
2020-04-26 21:32:29 -0400

    T17

---
+++ JAVA_tutorial/src/T17_classes_Employee.java
@@ -0,0 +1,11 @@
+
+public class T17_classes_Employee {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("Peter", "Jen");
+		System.out.println(e1.getFullname());
+
+
+	}
+
+}
