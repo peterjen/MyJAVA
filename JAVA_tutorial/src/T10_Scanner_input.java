8ff3cbdba97a2bcf2eb791ccaa3327eb852d8d27
Peter Jen <ptjen@yahoo.com>
2020-04-26 21:31:17 -0400

    T10

---
+++ JAVA_tutorial/src/T10_Scanner_input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class T10_Scanner_input {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String s = scanner.nextLine();
+		System.out.println(s);
+		System.out.println("Enter a int: ");
+		int i = scanner.nextInt();
+		System.out.println(i);
+		scanner.close();
+	}
+
+}
