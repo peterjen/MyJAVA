56562c6b4fff91328c6033c536c874df00d3d527
Peter Jen <ptjen@yahoo.com>
2020-04-27 17:23:22 -0400

    T22

--- HandsOnJAVA/src/GenericClass_D22.java
+++ HandsOnJAVA/src/GenericClass_D22.java
@@ -23,60 +23,12 @@ public class GenericClass_D22 {
 		System.out.println(itg_person.x.email);
 		System.out.println(itg_person.getX().name);
 		System.exit(0);
-		//IG1();
-		//IG2();
+		IG1();
+		
 		//genericWildCard();
 	
 	}
 
-	private static void genericWildCard() {
-		Admin a = new Admin();
-		a.email = "aaa.emai.com";
-		a.name = "AAA";
-		
-		ArrayList<Admin> admins = new ArrayList<Admin>();
-		admins.add(a);
-		@SuppressWarnings("unchecked")
-		// Convert Admins List to Person List
-		List<Person> ppp = (List<Person>)(List<?>)admins;
-
-		doSomething(ppp);
-		
-	}
-
-	private static void doSomething(List<Person> ppp) {
-		for (Person p: ppp) {
-			System.out.println(p.email + " " + p.name);
-		}
-		
-	}
-
-	private static void IG2() {
-		ItemGenericPair<String,String> igps1 = new ItemGenericPair<>();
-		igps1.setX("ssx1");
-		igps1.setY("ssy1");
-		System.out.println(igps1.getX());
-		System.out.println(igps1.getY());
-		ItemGenericPair<String,String> igps2 = new ItemGenericPair<>();
-		igps2.setX("ssx2");
-		igps2.setY("ssy2");
-		System.out.println(igps2.getX());
-		System.out.println(igps2.getY());
-		
-		ItemGenericPair<Integer,Integer> igpi = new ItemGenericPair<>();
-		igpi.setX(100);
-		igpi.setY(200);
-		System.out.println(igpi.getX());
-		System.out.println(igpi.getY());
-		
-		ArrayList<ItemGenericPair<String,String>> pairs = new ArrayList<>();
-		pairs.add(igps1);
-		pairs.add(igps2);
-		
-
-		
-		
-	}
 
 	private static void IG1() {
 		ItemGeneric<String> igs = new ItemGeneric<String>();  // Set Generic Class as String
